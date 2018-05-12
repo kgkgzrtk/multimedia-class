a7edb22895eccdd90a33e1166249de9ec7191c57
kgkgzrtk <kgkgzrtk@gmail.com>
2018-05-13 03:58:09 +0900

    updaate: add sway

--- fit.py
+++ fit.py
@@ -8,7 +8,7 @@ import glob
 RANK = 1000
 RADIUS = 3
 DIV = 16
-MIN_LOSS = 50
+MIN_LOSS = 1000
 
 # file read
 temp_files = glob.glob('template/*.ppm')
@@ -17,12 +17,14 @@ temp_imgs = {os.path.basename(t):cv2.imread(t) for t in temp_files}
 back_imgs = {os.path.basename(c):cv2.imread(c) for c in back_files}
 
 # filter
+fil_gaus = np.array([[1/16, 1/8, 1/16],[1/8,1/4,1/8],[1/16,1/8,1/16]])
 fil_y = np.array([[1.,2.,1.],[0.,0.,0.],[-1.,-2.,-1.]])
 fil_x = fil_y.transpose()
 
 # lambda fanc
 norm = lambda v: v/np.sum(v)
 argmax_2d = lambda x: list(zip(*np.unravel_index(np.argsort(x.ravel())[::-1],x.shape)))
+f_gaus = lambda img: np.dstack(signal.convolve2d(p, fil_gaus, 'same') for p in cv2.split(img))
 
 #def fanc
 def blank_to_zero(img):
@@ -31,15 +33,11 @@ def blank_to_zero(img):
 
 
 def get_fp(img, rank, rad=RADIUS):
-    fp = []
     h, w = img.shape[:2]
-    rad_area = np.zeros(img.shape)
     img = conv_rgb(img)
-    rgb = cv2.split(img)
-    for i,p in enumerate(rgb):
-        arg_list = argmax_2d(p)[:rank]
-        [fp.append((arg[0], arg[1], i)) for arg in arg_list]
-    return fp, img
+    arg_list = argmax_2d(img[:,:,0]+img[:,:,1]+img[:,:,2])[:rank]
+    
+    return arg_list, img
 
 
 def hist_rgb(img):
@@ -49,6 +47,7 @@ def hist_rgb(img):
 
 def conv_rgb(img):
     rgb = cv2.split(img)
+    rgb = (signal.convolve2d(p, fil_gaus, 'same') for p in rgb)
     rgb = (signal.convolve2d(p, fil_y, 'same') for p in rgb)
     res = (signal.convolve2d(p, fil_x, 'same') for p in rgb)
     return np.dstack(res)
@@ -61,7 +60,7 @@ def fp_writer(arg_list,shape, name='feature_point'):
     return point_img
 
 def make_feat_data(f_map, points, rad=RADIUS, scale=1):
-    div=DIV; spec=4;
+    div=DIV; spec=10;
     coords = []
     rad*=scale
     for i in range(div):
@@ -70,33 +69,42 @@ def make_feat_data(f_map, points, rad=RADIUS, scale=1):
         x = int(np.round(rad * np.cos(r*i)))
         coords.append((y,x))
         #print(y,x)
-
     d0, d1, d2 = f_map.shape
-    point_val = np.zeros([d0, d1, d2, div])
+    point_val = np.zeros((d0, d1, div, d2))
     new_p_li = []
     for p in points:
-        py,px,pc = p
+        py, px = p
         if py<rad or px<rad or py+rad>=d0 or px+rad>=d1: continue
-        arr = np.array([f_map[py+c[0],px+c[1],pc] for c in coords])
-        if np.abs(np.sum(arr>127)-int(div/2))<spec:
+        arr = np.array([f_map[py+c[0],px+c[1]] for c in coords])
+        if np.sum(arr>127)>spec:
             point_val[p] = arr
-            new_p_li.append(p)
+            if p not in new_p_li: new_p_li.append(p)
     return point_val, new_p_li
-"""
-def sway(data, div=DIV, i):
-    d_deg = i/(360/div)
-    [data[i+1] for i,d in enumerate(data[:-1])]
-"""
+
+def sway(data, d, div=DIV):
+    if d==0: return data
+    elif d<0 :
+        data=data[::-1]
+        dd=abs(d)
+    else: dd=d
+    d_deg = dd/(360/div)
+    data = np.append(data,[data[0]],axis=0)
+    data = np.array([d_deg*dat+(1-d_deg)*data[i+1] for i,dat in enumerate(data[:-1])])
+    if d<0 : return data[::-1]
+    else : return data
+
 def loss_cal(a,b):
     map(np.array,(a,b))
     if np.sum(b)-np.sum(a)>50: return np.inf, 0
     li = []
-    for i in range(len(b)):
-        b = np.roll(b,i)
+    deg_li = [i for i in range(-20,20+1)]
+    for i in deg_li:
+        b = sway(b,-i)
+        zero_rate = np.sum(b==0)/b.size
         b[b==0] = a[b==0]
-        li.append(np.linalg.norm(a-b)) 
+        li.append((1-zero_rate)*np.linalg.norm(a-b)) 
     arg = np.argmin(li)
-    return li[arg], arg-1
+    return li[arg], deg_li[arg]
 
 
 
@@ -135,11 +143,10 @@ def feat_mat(a_img, b_img, rank=RANK, scale=1):
     V1_NUM = 100
 
     # temp
-    conv_a = conv_rgb(a_img) 
-    feat_a = argmax_2d(conv_a)[:rank]
+    feat_a, conv_a = get_fp(a_img, rank=rank)
     ap_img = fp_writer(feat_a, a_img.shape, name='temp_feature')
-    a_val, feat_a = make_feat_data(a_img, feat_a)
-    cv2.imwrite('result/val_img.ppm', np.sum(a_val, axis=3))
+    a_val, feat_a = make_feat_data(f_gaus(a_img), feat_a)
+    cv2.imwrite('result/val_img.ppm', np.sum(a_val, axis=2))
 
     # back_image
     conv_b = conv_rgb(b_img)
@@ -151,53 +158,44 @@ def feat_mat(a_img, b_img, rank=RANK, scale=1):
     for ap in feat_a[:V1_NUM]: 
         for y in range(b_img.shape[0]):
             for x in range(b_img.shape[1]):
-                err = np.sum(np.abs(conv_b[y,x]-conv_a[ap[:2]]))
-                if err < 300:
-                    feat_b_paired.append((y,x,ap[2]))
+                bp = (y,x)
+                err = np.sum(np.abs(conv_b[y,x]-conv_a[ap]))
+                if err < 100 : 
+                    feat_b_paired.append(bp)
     
-    b_val, feat_b = make_feat_data(b_img, feat_b_paired, scale=scale)
-    print(feat_b)
-    cv2.imwrite('result/b_val_img.ppm', np.sum(b_val, axis=3))
+    b_val, feat_b = make_feat_data(f_gaus(b_img), feat_b_paired, scale=scale)
+    cv2.imwrite('result/b_val_img.ppm', np.sum(b_val, axis=2))
 
     min_loss = MIN_LOSS
     eps = 1e-10
     for ap in feat_a[:V1_NUM]:
         for bp in feat_b:
-            if ap[2] != bp[2]: continue
             loss, deg = loss_cal(a_val[ap], b_val[bp])
-            if min_loss>=loss:
+            if loss < MIN_LOSS:
                 vec_list.append({'a':np.array(ap), 'b':np.array(bp), 'loss':loss, 'deg':deg})
-                print("[add_vec] loss:%d deg:%f"%(loss,deg*22.5),ap,bp)
     mo = (0,0)
     if len(vec_list)<=2: return (mo, 0, 0)
 
-    f_vec_li = []
-    vec_list = sorted(vec_list, key=lambda x:x['loss'])
-    vec0_a = vec_list[0]['a'][:2]
-    vec0_b = vec_list[0]['b'][:2]
-    d_v = (vec0_b[0]-vec0_a[0],vec0_b[1]-vec0_a[1])
-
-    for vec in vec_list:
-        if np.linalg.norm(vec['a'][:2]-vec0_a)>3:
-            f_vec_li.append(vec)
-
-    vec_deg_li = []; vec_scale_li = []
+    f_vec_li = vec_list
+    vec_list = sorted(vec_list, key=lambda x:x['loss']) 
+    for v in vec_list[:20]:
+        print("[add_vec] loss:%d deg:%f"%(v['loss'],v['deg']),v['a'],v['b'])
+    d_vec_li = []; vec_deg_li = []
     for f_vec in f_vec_li:
-        vec_a = f_vec['a'][:2]-vec0_a
-        vec_b = f_vec['b'][:2]-vec0_b
-        vec_a_s, vec_b_s = map(np.linalg.norm,(vec_a,vec_b))
-        vec_deg_li.append(np.arccos(np.dot(vec_a,vec_b)/(vec_a_s*vec_b_s+eps)))
-        vec_scale_li.append(vec_b_s/vec_a_s)
-        print(vec_a,vec_b)
-    vec_deg = vec_deg_li[0]
-    vec_scale = vec_scale_li[0]
-    print(vec_scale,vec_deg)
-    return d_v, vec_scale, vec_deg
+        va = np.array(f_vec['b']); vb = np.array(f_vec['a'])
+        d_vec_li.append(vb-va)
+        vec_deg_li.append(f_vec['deg'])
+    npint = np.frompyfunc(int,1,1)
+    d_vec = npint(np.mean(d_vec_li, axis=0))
+    vec_deg = np.int(np.mean(vec_deg_li))
+    vec_scale = 1.
+    print(d_vec, vec_scale,vec_deg)
+    return d_vec, vec_scale, vec_deg
 
 # main-code
 
 ## load image
-temp_img = blank_to_zero(temp_imgs['inu.ppm'])
+temp_img = blank_to_zero(temp_imgs['reoinu.ppm'])
 back_img = back_imgs['class1_b1_n0_1.ppm']
 t_h, t_w = np.shape(temp_img)[:2]
 b_h, b_w = np.shape(back_img)[:2]
@@ -207,17 +205,15 @@ print('image_size:(%d,%d)'%(b_h,b_w))
 
 #plt.plot(temp_hist[0])
 
-
 a_img = temp_img
 b_img = back_img
-trim_img, cen_yx, size = hist_mat(a_img, b_img, 0.8)
+trim_img, cen_yx, size = hist_mat(a_img, b_img, 1.)
 c_y, c_x = cen_yx[0], cen_yx[1]; s_h, s_w = size[0], size[1]
 cv2.imwrite('result/hist_res_img.ppm', trim_img)
 
 move, rescale, redeg = feat_mat(a_img, trim_img, scale=0.8)
-print(move, rescale, redeg)
-c_y += move[0]
-c_x += move[1]
+c_y -= move[0]
+c_x -= move[1]
 fix_img = b_img[c_y:c_y+s_h, c_x:c_x+s_w]
 
 #rotation & resize
